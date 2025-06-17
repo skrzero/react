681427f53c2aaa02013379fe8cc5244b605f16b4
skrzero <Formationafec@outlook.fr>
2025-06-17 16:32:10 +0200

    FormulaireMoney

--- project_react/src/FormulaireMoney.jsx
+++ project_react/src/FormulaireMoney.jsx
@@ -1,6 +1,40 @@
-function FormulaireMoney(){
-    return(
-        <h1 class="text-3xl font-bold underline">test</h1>
-    )
+import { useEffect, useState } from "react";
+
+function FormulaireMoney() {
+    const [currencyData,setCurrencyData] = useState([])
+    const [result,SetResult] = useState(null)
+
+    useEffect(()=>{
+        fetch("https://grippy.learn.pierre-godino.com/api/mock/react-converter")
+        .then((response)=>{
+            return response.json()
+        })
+        .then((data)=>{
+         setCurrencyData(data.rates)
+        })
+        .catch(()=>{
+            SetResult("erreur de connection API")
+        })
+    },[])
+  return (
+    <>
+      <div>
+        <form>
+          <label className="ammount">Ammount</label>
+          <input type="text" className="Ammount" placeholder="ici.."  min={0} onChange={(e)=>setCurrencyData(e.target.value)}/>
+          <label className="search">Search Currenty</label>
+          <input type="text"className="Search Currenty" placeholder="ici..."/>
+          <ul className="liste">
+            {currencyData.map((currency,index)=>{
+                return <li key={index}>{currency.description} {currency.code}</li>
+            })}
+          </ul>
+          <div className="result">
+            <p>resultat</p>
+          </div>
+        </form>      
+      </div>
+    </>
+  );
 }
-export default FormulaireMoney
+export default FormulaireMoney;
